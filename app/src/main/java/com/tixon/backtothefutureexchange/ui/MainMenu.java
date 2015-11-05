ef9908b96ec9e966794054913c2c485d90069e15
Nikita <NikitaSD2010@ya.ru>
2015-11-06 00:31:36 +0300

    Пушка

--- app/src/main/java/com/tixon/backtothefutureexchange/ui/MainMenu.java
+++ app/src/main/java/com/tixon/backtothefutureexchange/ui/MainMenu.java
@@ -4,34 +4,34 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-
+import android.widget.TextView;
 
 
 import com.tixon.backtothefutureexchange.Constants;
+import com.tixon.backtothefutureexchange.R;
 
-public class MainMenu extends AppCompatActivity
-{
-    private Button new_game_b1, continue_b2, options_b3, exit_b4;
+public class MainMenu extends AppCompatActivity {
 
-    private void SetTypeface()
-    {
-        new_game_b1.setTypeface(Typeface.createFromAsset(getResources().getAssets(), Constants.TYPEFACE_DIGITS));
-        continue_b2.setTypeface(Typeface.createFromAsset(getResources().getAssets(), Constants.TYPEFACE_DIGITS));
-        options_b3.setTypeface(Typeface.createFromAsset(getResources().getAssets(), Constants.TYPEFACE_DIGITS));
-        exit_b4.setTypeface(Typeface.createFromAsset(getResources().getAssets(), Constants.TYPEFACE_DIGITS));
 
-    }
-    private void SetColor()
-    {
-        new_game_b1.setTextColor(ControlPanelItem.GREEN);
-        continue_b2.setTextColor(ControlPanelItem.GREEN);
-        options_b3.setTextColor(ControlPanelItem.GREEN);
-        exit_b4.setTextColor(ControlPanelItem.GREEN);
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+
+        TextView tv = (TextView) findViewById(R.id.ng1);
+        Typeface face = Typeface.createFromAsset(getAssets(), "ds_digit_font.ttf");
+        tv.setTypeface(face);
+
+        TextView tv1 = (TextView) findViewById(R.id.ct2);
+        Typeface face1 = Typeface.createFromAsset(getAssets(), "ds_digit_font.ttf");
+        tv1.setTypeface(face1);
+
+        TextView tv2 = (TextView) findViewById(R.id.op3);
+        Typeface face2 = Typeface.createFromAsset(getAssets(), "ds_digit_font.ttf");
+        tv2.setTypeface(face2);
+
+        TextView tv3 = (TextView) findViewById(R.id.ex4);
+        Typeface face3 = Typeface.createFromAsset(getAssets(), "ds_digit_font.ttf");
+        tv3.setTypeface(face3);
     }
 }
